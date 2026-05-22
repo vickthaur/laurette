4dc7bda9ae34e58a9ddc48ffe90b2e2f4d015ea2
Victor <victordetroux2@gmail.com>
2026-05-22 11:24:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
 // ==========================================
 // 1. SUPABASE & ÉTAT GLOBAL
 // ==========================================
-const SUPABASE_URL  = 'https://lhelfggczeybornhemsf.supabase.co/rest/v1/';
-const SUPABASE_KEY  = 'sb_publishable_rvOSgbkjsZRcwGCenkzY_g_5am-b6dk';
+const SUPABASE_URL  = "https://lhelfggczeybornhemsf.supabase.co/rest/v1/";
+const SUPABASE_KEY  = "sb_publishable_rvOSgbkjsZRcwGCenkzY_g_5am-b6dk";
 
 let supabase;
 try {
